f98b316fb333a9b9890d2befbc20b441070b79ad
Nirajan Basnet <er.nirajan.basnet@gmail.com>
2019-07-11 22:19:57 +0545

    admin login functionality added

--- themes/demoghagvid/src/js/app.js
+++ themes/demoghagvid/src/js/app.js
@@ -5,44 +5,44 @@ $('.modal-toggle').on('click', function(e) {
   $('.modal').toggleClass('is-visible');
 });
 
-// Color Picker Tool Js
-const themeSwitchers = document.querySelectorAll('span');
-const dynamicInputs = document.querySelectorAll('input.input-color-picker');
-
-const handleThemeUpdate = (cssVars) => {
-  const root = document.querySelector(':root');
-  const keys = Object.keys(cssVars);
-  keys.forEach(key => {
-    root.style.setProperty(key, cssVars[key]);
-    $.fn.changeColorForm(key, cssVars[key])
-  });
-}
-
-themeSwitchers.forEach((item) => {
-  item.addEventListener('click', (e) => {
-    const bgColor = e.target.getAttribute('data-bg-color')
-    const color = e.target.getAttribute('data-color')
-    handleThemeUpdate({
-      '--primary-bg-color': bgColor,
-      '--primary-color': color
+$(document).ready(function () {
+  // Color Picker Tool Js
+  const themeSwitchers = document.querySelectorAll('span');
+  const dynamicInputs = document.querySelectorAll('input.input-color-picker');
+  
+  const handleThemeUpdate = (cssVars) => {
+    const root = document.querySelector(':root');
+    const keys = Object.keys(cssVars);
+    keys.forEach(key => {
+      root.style.setProperty(key, cssVars[key]);
+      $.fn.changeColorForm(key, cssVars[key])
     });
-    
-    console.log(bgColor, color)
-    $("input.input-color-picker[data-id='color']").val(color)
-    $("input.input-color-picker[data-id='bg-color']").val(bgColor)
-  });
-});
-
-dynamicInputs.forEach((item) => {
-  item.addEventListener('input', (e) => {
-    const cssPropName = `--primary-${e.target.getAttribute('data-id')}`;
-    console.log(cssPropName)
-    handleThemeUpdate({
-      [cssPropName]: e.target.value
+  }
+  
+  themeSwitchers.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      const bgColor = e.target.getAttribute('data-bg-color')
+      const color = e.target.getAttribute('data-color')
+      handleThemeUpdate({
+        '--primary-bg-color': bgColor,
+        '--primary-color': color
+      });
+      $("input.input-color-picker[data-id='color']").val(color)
+      $("input.input-color-picker[data-id='bg-color']").val(bgColor)
     });
   });
+  
+  dynamicInputs.forEach((item) => {
+    item.addEventListener('input', (e) => {
+      const cssPropName = `--primary-${e.target.getAttribute('data-id')}`;
+      handleThemeUpdate({
+        [cssPropName]: e.target.value
+      });
+    });
+  });
+  
+  $.fn.changeColorForm = function (key, color) {
+    $(`form[name=contact] input[name='${key}']`).val(color)
+  };
+  
 });
-
-$.fn.changeColorForm = function (key, color) {
-  $(`form[name=contact] input[name='${key}']`).val(color)
-};
